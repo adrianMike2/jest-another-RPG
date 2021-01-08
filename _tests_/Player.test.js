827cfd8e33f61681e1e91958dfe1a8043be368ed
adrian <sargentamp149@gmail.com>
2021-01-08 12:59:36 -0500

    player stats are working

---
+++ _tests_/Player.test.js
@@ -0,0 +1,27 @@
+const Player = require('../lib/Player');
+jest.mock('../lib/Potion');
+console.log(new Potion());
+
+function Potion(name) {
+    this.types = ['strength', 'agility', 'health'];
+    this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
+
+    if (this.name === 'health') {
+        this.value = Math.floor(Math.random() * 10 + 30);
+    } else {
+        this.value = Math.floor(Math.random() * 5 + 7);
+    }
+}
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+});
+
+module.exports = Potion;
